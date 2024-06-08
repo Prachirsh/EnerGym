15253e7b9e3537c6df6bedd7709d42f8b81a6aa0
Prachi Shintre <120733226+Prachirsh@users.noreply.github.com>
2024-06-08 15:28:45 +0530

    Update dbh.php

--- includes/dbh.php
+++ includes/dbh.php
@@ -1,8 +1,16 @@
 <?php
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:gym1.database.windows.net,1433; Database = energym", "Azureuser", "123456");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
 
-$dbServername = "localhost";
-$dbUsername = "root";
-$dbPassword = "";
-$dbName = "energym";
-
-$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName );
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "Azureuser", "pwd" => "123456", "Database" => "energym", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:gym1.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+?>
