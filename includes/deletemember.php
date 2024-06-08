e0527c62e4628406f43037cb5cf3f9a8ab1ddf06
Prachi Shintre <120733226+Prachirsh@users.noreply.github.com>
2024-06-08 15:32:08 +0530

    Update deletemember.php

--- includes/deletemember.php
+++ includes/deletemember.php
@@ -1,5 +1,18 @@
 <?php
-$con=mysqli_connect("localhost","root","","energym");
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:gym1.database.windows.net,1433; Database = energym", "Azureuser", "{your_password_here}");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "Azureuser", "pwd" => "{your_password_here}", "Database" => "energym", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:gym1.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
 $id = $_GET['deleteid'];
     $qry = mysqli_query($con,"delete  from members where member_id='$id'");
     if ($qry)
@@ -7,3 +20,4 @@ $id = $_GET['deleteid'];
 echo '<script>alert("deleted successfully");location.href="../member_details.php";</script>';
     }
 ?>
+
