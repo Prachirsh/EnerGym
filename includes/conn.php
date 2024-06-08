1bfc41e55f5c0072676594aa04a3e2ac50e9a2b8
Prachi Shintre <120733226+Prachirsh@users.noreply.github.com>
2024-06-08 15:23:44 +0530

    Update conn.php

--- includes/conn.php
+++ includes/conn.php
@@ -1,12 +1,16 @@
 <?php
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:gym1.database.windows.net,1433; Database = energym", "Azureuser", "{your_password_here}");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
 
-// php select option value from database
-
-$hostname = "localhost";
-$username = "root";
-$password = "";
-$databaseName = "energym";
-$connect = mysqli_connect($hostname, $username, $password, $databaseName);
-$query = "SELECT * FROM `Trainer`";
-$result1 = mysqli_query($connect, $query);
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "Azureuser", "pwd" => "{your_password_here}", "Database" => "energym", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:gym1.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
 ?>
