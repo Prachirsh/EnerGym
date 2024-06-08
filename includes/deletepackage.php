c4b9526aa7d203e3e7c1ed1ec4640ddbb6b77c72
Prachi Shintre <120733226+Prachirsh@users.noreply.github.com>
2024-06-08 15:33:54 +0530

    Update deletepackage.php

--- includes/deletepackage.php
+++ includes/deletepackage.php
@@ -1,5 +1,18 @@
 <?php
-$con=mysqli_connect("localhost","root","","energym");
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:gym1.database.windows.net,1433; Database = energym", "Azureuser", "{your_password_here}");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "Azureuser", "pwd" => "{your_password_here}", "Database" => "energym", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:gym1.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
 $id = $_GET['deleteid'];
     $qry = mysqli_query($con,"delete  from package where Package_id='$id'");
     if ($qry)
