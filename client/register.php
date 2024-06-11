f27083db8c0985acfcb225133c41803a75d3b2ad
Prachi Shintre <120733226+Prachirsh@users.noreply.github.com>
2024-06-11 12:48:32 +0530

    Update register.php

--- client/register.php
+++ client/register.php
@@ -44,13 +44,33 @@ include "../includes/imports.php";
             <option value="<?php echo $row1[0];?>"><?php echo $row1[1];?></option>
             <?php endwhile;?>
             <option value="">No Trainer</option>
-        </select>
-        <br>       
-        <input type="submit" class="btn btn-primary" name="customer_submit" value="Register" /> 
-          
-       <a href="../index.php" class="btn btn-danger">Back</a>
+        </select> 
+        <br>      
+  
+            <?php
+        if(array_key_exists('button1', $_POST)) { 
+            button1(); 
+        } 
+        else if(array_key_exists('button2', $_POST)) { 
+            button2(); 
+        }
 
+ 
+function button1() { 
+            echo "This is Button1 that is selected"; 
+        } 
+        function button2() { 
+            echo "This is Button2 that is selected"; 
+        } 
+<form method="post"> 
+        <input type="submit" name="button1"
+                class="button" value="Button1" /> 
+          
+        <input type="submit" name="button2"
+                class="button" value="Button2" /> 
     </form>
+    ?>        
+
   <div class="mt-5"></div>
       <?php 
       include '../includes/footer.php';
