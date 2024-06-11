4ee409a94f402a696fd28d716373b5d57feb330c
Prachi Shintre <120733226+Prachirsh@users.noreply.github.com>
2024-06-11 04:24:54 +0000

    connection code edit for function.php

--- function.php
+++ function.php
@@ -4,7 +4,19 @@
        session_start(); 
    } 
 error_reporting(0);
-$con=mysqli_connect("localhost","root","","energym");
+try {
+    $conn = new PDO("sqlsrv:server = tcp:gym1.database.windows.net,1433; Database = energym", "Azureuser", "{your_password_here}");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "Azureuser", "pwd" => "{your_password_here}", "Database" => "energym", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:gym1.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
 //checking login
 if(isset($_POST['login_submit'])){
 	$username=$_POST['username'];
